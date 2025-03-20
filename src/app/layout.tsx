929f3bd5d0351e05079cdd6bfe29dc7e46f48c30
ronodongo <ronaldodongo27@gmail.com>
2025-03-20 17:52:02 +0300

    add layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import "~/styles/globals.css"; 
+import Header from "~/components/Header";
+import Footer from "~/components/Footer";
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Blingers Empire",
+  description: "Welcome to Blingers Empire - Dance Company",
+  icons: [{ rel: "icon", url: "/covers/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${GeistSans.variable}`}>
+      <body className="bg-[#f8f9fa] text-[#1a3264] min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-grow pt-20">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
